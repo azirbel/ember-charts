08f54a14c6d9ccc41d7810ae6354fccd76898e4e
Alex Zirbel <alex.zirbel@addepar.com>
2014-10-28 23:58:28 -0700

    Release 0.3.5

--- dist/ember-charts.js
+++ dist/ember-charts.js
@@ -1,5 +1,5 @@
 /*!
-* ember-charts v0.3.4
+* ember-charts v0.3.5
 * Copyright 2012-2014 Addepar Inc.
 * See LICENSE.
 */
@@ -159,7 +159,7 @@ Ember.AddeparMixins.StyleBindingsMixin = Ember.Mixin.create({
 
 Ember.Charts = Ember.Namespace.create();
 
-Ember.Charts.VERSION = '0.3.4';
+Ember.Charts.VERSION = '0.3.5';
 
 if ((_ref = Ember.libraries) != null) {
   _ref.register('Ember Charts', Ember.Charts.VERSION);
